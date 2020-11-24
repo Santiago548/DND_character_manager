8b2dbe25681aa2d6edf11a9ff96bc98a0f0148cc
Santiago548 <dominicbarajas548@gmail.com>
2020-11-24 12:48:08 -0700

    added migration tables for players and characters

--- db/migrate/20201124183719_create_characters.rb
+++ db/migrate/20201124183719_create_characters.rb
@@ -1,8 +1,20 @@
 class CreateCharacters < ActiveRecord::Migration
   def change
-    create_table :charcters do |t|
-      t.string :charcter_name
+    create_table :characters do |t|
+      t.string :character_name
       t.integer :character_level
-      t.string
+      t.string :character_race
+      t.string :character_class
+      t.integer :armor_class
+      t.integer :character_speed
+      t.integer :character_strength
+      t.integer :character_dexterity
+      t.integer :character_constitution
+      t.integer :character_intelligence
+      t.integer :character_wisdom
+      t.integer :character_charisma
+      t.string :weapon_proficiencies
+      t.string :armor_proficiencies
+      t.string :languages
   end
 end
