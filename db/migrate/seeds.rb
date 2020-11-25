7cc007aa50dc4979e7602957b98d9f10e69c0a33
Santiago548 <dominicbarajas548@gmail.com>
2020-11-25 14:15:51 -0700

    added seed data for 1 player and character

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,9 @@
+sabrina = Owner.create(username: "Boots1234", name: "Sabrina", age: "27", birthdate: "11/26/1993")
+Character.create(name: "Maddy", owner: sophie)
+Character.create(name: "Nona", owner: sophie)
+
+
+character_name: "Mac", character_level: 12, character_race: "Elf", character_class: "Ranger", armor_class: 18,
+character_speed: 45, character_strength: 3, character_dexterity: 4, character_constitution: 3,
+character_intelligence: 2, character_wisdom: 3, character_charisma: 3, weapon_proficiencies: "",
+armor_proficiencies: "", languages: "", player_id: 1)
