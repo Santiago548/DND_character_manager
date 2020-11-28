186e5bd7202acf52551221d47ae7d0195e97a7c1
Santiago548 <dominicbarajas548@gmail.com>
2020-11-28 09:33:35 -0700

    added three players and characters to the seed.rb file for db testing

--- db/migrate/20201124181448_create_players.rb
+++ db/migrate/20201124181448_create_players.rb
@@ -2,6 +2,7 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.string :username
+      t.string :email
       t.string :name
       t.integer :age
       t.date :birthdate
--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -1,9 +1,10 @@
-sabrina = Owner.create(username: "Boots1234", name: "Sabrina", age: "27", birthdate: "11/26/1993")
-Character.create(name: "Maddy", owner: sophie)
-Character.create(name: "Nona", owner: sophie)
+sabrina = Player.create(username: "boots1234", email: "sabrina@gmail.com", name: "Sabrina", age: "27", birthdate: "11/26/1993")
+dominic = Player.create(username: "santiago1234", email: "dominic@gmail.com", name: "Dominic", age: "30", birthdate: "02/06/1990")
+thomas = Player.create(username: "dngrb4x", email: "thomas@gmail.com". name: "Thomas", age: "32", birthdate: "06/12/19888")
 
+Character.create(character_name: "Mac", character_level: 12, character_race: "Elvish", character_class: "Ranger", armor_class: 18,
+character_speed: 45, character_strength: 3, character_dexterity: 4, character_constitution: 3, character_intelligence: 2, character_wisdom: 3, character_charisma: 3, weapon_proficiencies: "long-swords", armor_proficiencies: "light-armor", languages: "Elvish", player_id: 1)
 
-character_name: "Mac", character_level: 12, character_race: "Elf", character_class: "Ranger", armor_class: 18,
-character_speed: 45, character_strength: 3, character_dexterity: 4, character_constitution: 3,
-character_intelligence: 2, character_wisdom: 3, character_charisma: 3, weapon_proficiencies: "",
-armor_proficiencies: "", languages: "", player_id: 1)
+Character.create(character_name: "Griff", character_level: 17, character_race: "Human", character_class: "Investigator", armor_class: 17, character_speed: 35, character_strength: 2, character_dexterity: 4, character_constitution: 3, character_intelligence: 5, character_wisdom: 3, character_charisma: 4, weapon_proficiencies: "firearms", armor_proficiencies: "light-armor", languages: "Common", player_id: 2)
+
+Character.create(character_name: "Mac", character_level: 12, character_race: "Elvish", character_class: "Ranger", armor_class: 18, character_speed: 45, character_strength: 3, character_dexterity: 4, character_constitution: 3, character_intelligence: 2, character_wisdom: 3, character_charisma: 3, weapon_proficiencies: "long-swords", armor_proficiencies: "light-armor", languages: "Elvish", player_id: 1)
