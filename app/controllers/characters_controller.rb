aeaf9a451b73f184adee2b2f87e0350989ca7eeb
Santiago548 <dominicbarajas548@gmail.com>
2020-12-06 16:33:56 -0700

    ability to edit characters from the db, and added multiple options to character options that have multiple choices

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -14,15 +14,19 @@ class CharactersController < ApplicationController
   end
 
   get '/character/:id' do
-  
+    @character = Character.find(params[:id])
+    erb :'characters/show'
   end
 
   get '/character/:id/edit' do
-  
+    @character = Character.find(params[:id])
+    erb :'/characters/edit'
   end
 
   patch '/character/:id' do
-
+    character = Character.find_by_id(params[:id])
+    character.update(params[:player])
+    redirect to "/character/#{character.id}"
   end
 
   delete '/character/:id' do
